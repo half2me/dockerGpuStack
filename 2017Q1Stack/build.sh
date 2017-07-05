522314ff8f4d8e489e038bdde8c01cc7a00c1bc5
libo zhu <libo.zhu@intel.com>
2017-07-06 00:22:57 +0800

    add intel-vaapi-driver and rename the image to ubuntu_intel_gpu_stack:2017q1
    Signed-off-by: libo zhu <libo.zhu@intel.com>

--- 2017Q1Stack/build.sh
+++ 2017Q1Stack/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker build --build-arg http_proxy="http://proxy-shz.intel.com:911/" -t ubuntu_with_opensource_drivers .
+sudo docker build --build-arg http_proxy="http://proxy-shz.intel.com:911/" -t ubuntu_intel_gpu_stack:2017q1 .
