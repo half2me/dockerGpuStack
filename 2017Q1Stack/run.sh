9e189e7a372605979af4b948836db8c9c70f3987
libo zhu <libo.zhu@intel.com>
2017-07-06 19:07:53 +0800

    use media as default
    Signed-off-by: libo zhu <libo.zhu@intel.com>

--- 2017Q1Stack/run.sh
+++ 2017Q1Stack/run.sh
@@ -2,5 +2,8 @@
 
 COMMAND=/bin/bash
 
-xhost + # allow connections to X server
-docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -it ubuntu_intel_gpu_stack:2017q1 $COMMAND
+#media
+docker run -it --device /dev/dri/renderD128 ubuntu_intel_gpu_stack:2017q1 $COMMAND
+#display X 
+#xhost + # allow connections to X server
+#docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -it ubuntu_intel_gpu_stack:2017q1 $COMMAND
